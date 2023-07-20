6352194c5d6526eb08dcd165e2833c21edbb1f46
Henzyd <henzydee@gmail.com>
2023-07-20 03:14:02 +0100

    chore: created `EmptyPost` component

--- client/src/components/AppContainer.tsx
+++ client/src/components/AppContainer.tsx
@@ -10,7 +10,7 @@ const AppContainer: React.FC<AppContainerProps> = ({ children }) => {
   return (
     <section
       id="AppContainer"
-      className="grid grid-cols-[1fr,3fr,1fr] gap-8 p-7 px-12 h-[inherit] max-w-screen-LargeLaptop mx-auto"
+      className="grid grid-cols-[1fr,3fr,1fr] gap-8 p-7 px-12 h-[inherit] max-w-screen-LargeLaptop mx-auto pb-12"
     >
       <LeftSide />
       <main className="flex flex-col">{children}</main>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,20 @@
-import CreatePost from "../components/CreatePost";
 import Seo from "../utils/Seo";
+import CreatePost from "../components/CreatePost";
+import EmptyPost from "../components/EmptyPost";
 
 const Home = () => {
+  const postsData = [];
   return (
     <>
       <Seo title="Home" description="Home page" />
       <main className="flex flex-col gap-4">
         <CreatePost />
         <hr className="h-[1px] bg-neutral-400 rounded-full w-[86%] mx-auto" />
+        {postsData.length >= 1 ? (
+          <div className="flex flex-col gap-4"></div>
+        ) : (
+          <EmptyPost />
+        )}
       </main>
     </>
   );
