7037895c6e5e37bfa7d4032981bfe3400b82119c
Henzyd <henzydee@gmail.com>
2023-08-03 02:02:02 +0100

    chore: implemented signup functionality

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import Seo from "../utils/Seo";
 import Input from "../components/Input";
 import Button from "../components/Button";
@@ -11,108 +13,63 @@ import AuthContainer from "../components/AuthContainer";
 import { useSignupMutation } from "../api/queries/authQuery";
 import { notifyError, notifySuccess } from "../utils/Toast";
 
+const signupSchema = Yup.object().shape({
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().email("Email is not valid").required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required and must be at least 8 characters"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password")], "Password and confirm password must match")
+    .required("Confirm password is required and must be at least 8 characters"),
+});
+
 const Signup = () => {
   const navigate = useNavigate();
-  const [userInput, setUserInput] = useState({
-    username: {
-      value: "",
-      error: false,
-    },
-    email: {
-      value: "",
-      error: false,
-    },
-    password: {
-      value: "",
-      error: false,
-    },
-    confirmPassword: {
-      value: "",
-      error: false,
-    },
-  });
   const [showPassword, setShowPassword] = useState({
     password: false,
     confirmPassword: false,
   });
 
-  const [signup, { isLoading, isSuccess, isError, error }] =
-    useSignupMutation();
+  const [signup, { isLoading }] = useSignupMutation();
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    console.log(userInput);
-
-    //? Validation
-    if (
-      !userInput.username.value ||
-      !userInput.email.value ||
-      userInput.password.value.length < 8 ||
-      userInput.confirmPassword.value.length < 8
-    ) {
-      setUserInput((prev) => ({
-        ...prev,
-        username: {
-          ...prev.username,
-          error: !prev.username.value ? true : false,
-        },
-        email: {
-          ...prev.email,
-          error: !prev.email.value ? true : false,
-        },
-        password: {
-          ...prev.password,
-          error: prev.password.value.length < 8 ? true : false,
-        },
-        confirmPassword: {
-          ...prev.confirmPassword,
-          error: prev.confirmPassword.value.length < 8 ? true : false,
-        },
-      }));
-      return;
-    } else {
-      setUserInput((prev) => ({
-        ...prev,
-        username: {
-          ...prev.username,
-          error: false,
-        },
-        email: {
-          ...prev.email,
-          error: false,
-        },
-        password: {
-          ...prev.password,
-          error: false,
-        },
-        confirmPassword: {
-          ...prev.confirmPassword,
-          error: false,
-        },
-      }));
-    }
-
-    await signup({
-      email: userInput.email.value,
-      password: userInput.password.value,
-      username: userInput.username.value,
-    });
-  }
-
-  useEffect(() => {
-    if (isSuccess) {
-      navigate("/login");
-      notifySuccess("Signup successful, please login");
-    }
-
-    if (error) {
-      if ("status" in error) {
-        if (error.status === 400) {
-          notifyError("Signup failed, email or username already exists");
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: signupSchema,
+    onSubmit: async (values) => {
+      try {
+        await signup({
+          email: values.email,
+          password: values.password,
+          username: values.username,
+        }).unwrap();
+        navigate("/login");
+        notifySuccess("Signedup successfully, please login");
+      } catch (error: any) {
+        if ("status" in error) {
+          if (error.status === 400) {
+            if (error.data.validationErrors) {
+              notifyError(error.data.validationErrors[0].message);
+            } else if (error.data.message) {
+              const message: string = error.data.message.toLowerCase();
+              if (message.includes("duplicate")) {
+                if (message.includes("email")) {
+                  notifyError("Signup failed, Email already exists");
+                } else if (message.includes("username")) {
+                  notifyError("Signup failed, Username already exists");
+                }
+              }
+            }
+          }
         }
       }
-    }
-  }, [isSuccess, isError]);
+    },
+  });
 
   return (
     <>
@@ -122,66 +79,52 @@ const Signup = () => {
         illustrationImgAlt="signup"
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={formik.handleSubmit}
           className="flex flex-col justify-center items-center w-full gap-[1rem]"
         >
           <h1 className="text-[1.5rem] font-bold mb-2 text-center">
-            Create an account
+            Create an Account
           </h1>
           <Input
             type="text"
-            onChange={(e) =>
-              setUserInput((prev) => ({
-                ...prev,
-                username: {
-                  ...prev.username,
-                  value: e.target.value,
-                },
-              }))
-            }
-            value={userInput.username.value}
+            onChange={formik.handleChange}
+            value={formik.values.username}
             id="login-name-input"
             label={"Username"}
-            name="name"
-            error={userInput.username.error}
-            helperText={"Username is required"}
+            name="username"
+            error={formik.touched.username && Boolean(formik.errors.username)}
+            helperText={
+              formik.touched.username && formik.errors.username
+                ? formik.errors.username
+                : null
+            }
           />
           <Input
             type="email"
-            onChange={(e) =>
-              setUserInput((prev) => ({
-                ...prev,
-                email: {
-                  ...prev.email,
-                  value: e.target.value,
-                },
-              }))
-            }
-            value={userInput.email.value}
+            onChange={formik.handleChange}
+            value={formik.values.email}
             id="login-email-input"
             label={"Email"}
             name="email"
-            error={userInput.email.error}
-            helperText={"Email is required"}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={
+              formik.touched.email && formik.errors.email
+                ? formik.errors.email
+                : null
+            }
           />
           <Input
             type={showPassword.password ? "text" : "password"}
-            onChange={(e) =>
-              setUserInput((prev) => ({
-                ...prev,
-                password: {
-                  ...prev.password,
-                  value: e.target.value.trim(),
-                },
-              }))
-            }
-            value={userInput.password.value}
+            onChange={formik.handleChange}
+            value={formik.values.password}
             id="login-password-input"
             label={"Password"}
             name="password"
-            error={userInput.password.error}
+            error={formik.touched.password && Boolean(formik.errors.password)}
             helperText={
-              "Password is required and must be at least 8 characters"
+              formik.touched.password && formik.errors.password
+                ? formik.errors.password
+                : null
             }
             endAdornment={
               <IconButton
@@ -200,22 +143,19 @@ const Signup = () => {
           />
           <Input
             type={showPassword.confirmPassword ? "text" : "password"}
-            onChange={(e) =>
-              setUserInput((prev) => ({
-                ...prev,
-                confirmPassword: {
-                  ...prev.confirmPassword,
-                  value: e.target.value.trim(),
-                },
-              }))
-            }
-            value={userInput.confirmPassword.value}
+            onChange={formik.handleChange}
+            value={formik.values.confirmPassword}
             id="login-confirm-password-input"
             label={"Confirm Password"}
             name="confirmPassword"
-            error={userInput.confirmPassword.error}
+            error={
+              formik.touched.confirmPassword &&
+              Boolean(formik.errors.confirmPassword)
+            }
             helperText={
-              "Confirm password is required and must be at least 8 characters"
+              formik.touched.confirmPassword && formik.errors.confirmPassword
+                ? formik.errors.confirmPassword
+                : null
             }
             endAdornment={
               <IconButton
@@ -236,7 +176,6 @@ const Signup = () => {
               </IconButton>
             }
           />
-
           <Button
             variant="contained"
             color="info"
