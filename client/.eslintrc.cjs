29e2716e5c2345c2f0c88e3c89a62f8122c892bd
Henzyd <henzydee@gmail.com>
2023-08-21 19:46:19 +0100

    refactored button component and added new eslint rules

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,16 +1,27 @@
-const config = {
+module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
   ],
+  ignorePatterns: ["public", "dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "no-console": [
+      "error",
+      {
+        allow: ["warn", "error"],
+      },
+    ],
   },
 };
-
-export default config;
