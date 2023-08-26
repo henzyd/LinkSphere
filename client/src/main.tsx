d8339bd4e62a4bcb381bd50f4fb44fc428cca423
Henzyd <henzydee@gmail.com>
2023-08-26 03:09:16 +0100

    fix: build error by update import format

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,12 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
-import theme from "./utils/theme.ts";
-import store from "./redux/store.ts";
-import LazyLoader from "./components/LazyLoader.tsx";
+import theme from "./utils/theme";
+import store from "./redux/store";
+import LazyLoader from "./components/LazyLoader";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
