bc1727edefee6b7639c1b246e4f46a6268ad6dd8
henzyd <henzydee@gmail.com>
2023-10-29 17:11:40 +0100

    refactored redux folder

--- client/src/redux/api/auth/index.ts
+++ client/src/redux/api/auth/index.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { API_BASE_URL } from "~/api";
+import { API_BASE_URL } from "~/utils/constants";
 
 const authApi = createApi({
-  reducerPath: "authApi",
+  reducerPath: "auth",
   baseQuery: fetchBaseQuery({
     baseUrl: `${API_BASE_URL}/auth`,
   }),
